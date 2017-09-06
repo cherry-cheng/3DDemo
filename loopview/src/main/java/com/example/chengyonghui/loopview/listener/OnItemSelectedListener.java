5c6b0c4cb91b1fca6a9c2e9b404877cbfccd9059
cherry-cheng <708923694@qq.com>
2017-09-06 17:53:16 +0800

    first commit

---
+++ loopview/src/main/java/com/example/chengyonghui/loopview/listener/OnItemSelectedListener.java
@@ -0,0 +1,13 @@
+package com.example.chengyonghui.loopview.listener;
+
+/**
+ * Created by chengyonghui on 2017/8/28.
+ */
+import android.view.View;
+
+/**
+ * 选择回调接口
+ */
+public interface OnItemSelectedListener {
+    void selected(int item, View view);
+}
---
+++ loopview/src/main/java/com/example/chengyonghui/loopview/listener/OnLoopViewTouchListener.java
@@ -0,0 +1,13 @@
+package com.example.chengyonghui.loopview.listener;
+
+/**
+ * Created by chengyonghui on 2017/8/28.
+ */
+import android.view.MotionEvent;
+
+/**
+ * 选择回调接口
+ */
+public interface OnLoopViewTouchListener {
+    void onTouch(MotionEvent event);
+}
